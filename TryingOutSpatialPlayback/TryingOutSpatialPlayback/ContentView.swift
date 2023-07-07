7840b1dd4c97a9209fe457c7beac5dfb87c4354f
Emma Walker <walkere@tcd.ie>
2023-07-07 09:20:00 +0100

    Adds repo to play with spatial playback

---
+++ TryingOutSpatialPlayback/TryingOutSpatialPlayback/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TryingOutSpatialPlayback
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 07/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TryingOutSpatialPlayback/TryingOutSpatialPlayback/TryingOutSpatialPlaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryingOutSpatialPlaybackApp.swift
+//  TryingOutSpatialPlayback
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TryingOutSpatialPlaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
